3ceeb584871ac033777468208a8cbd1c22733e6c
yuhaikuo <yuhaikuo231@gmail.com>
2017-06-15 01:10:59 +0800

    构建简单restful api

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -18,7 +18,7 @@ class HomeController extends BaseController
     public function home()
     {
         $re = $this->model->getOne();
-        $this->view = View::make('home')->with('home', $re);
+        $this->sendResponse($re);
     }
 
     public function sendMailHand()
